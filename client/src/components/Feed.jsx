a8de4066e152c32973f8137ea36433f45f166eb1
George Vazquez <george.h.vazquez@gmail.com>
2023-03-07 16:57:27 -0500

    logic and dummy data added

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -3,14 +3,24 @@ import { UserCard } from "../components/UserCard.jsx";
 
 export const Feed = () => {
     // logic here for amount of user cards needed
+    const cards = [];
+
+    const names = ["alex", "chris", "george", "nancy", "time"];
+
+    for(let i = 0; i < names.length; i++){
+      cards.push(<UserCard
+        key={i} 
+        name={names[i]}
+        match={'100%'}
+        skills={'react'}
+      />);
+    }
     return (
       <>
         <div className="feed-container">
             <h1>Feed</h1>
             <div className="card-container">
-                <UserCard/>
-                <UserCard/>
-                <UserCard/>
+              { cards }
             </div>
         </div>
       </>  
