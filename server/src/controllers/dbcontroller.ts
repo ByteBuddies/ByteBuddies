deb7f1015b328dfcc69be4aa50282da5ea8518a7
tk-0311 <113018868+tk-0311@users.noreply.github.com>
2023-03-06 22:22:01 -0500

    progress creating db user controller

--- server/src/controllers/dbcontroller.ts
+++ server/src/controllers/dbcontroller.ts
@@ -1,21 +1,49 @@
 import db from '../models/userModels'
+import * as T from '../../type'
 import { Request, Response, NextFunction } from 'express'
 export default {
   getUser: async (req: Request,res: Response, next: NextFunction):Promise<any> => {
     try {
       const {username} = req.body
-      const command = "SELECT * FROM user WHERE username=$1"
+      const command:string = "SELECT b.username, p. FROM bytes b INNER JOIN profile p ON p.profile_id=b.profile_id WHERE username=$1"
       const user = await db.query(command, [username])
+      res.locals.user = user
     }catch(err:any) {
-
+       const error:T.error = {
+        message:`/controllers/dbController getUser error ${typeof err === 'object' ? JSON.stringify(err) : err}`,
+        status: 500,
+        log: 'database error check server log'
+      }
+      return next(error)
     }
   },
   createUser: (req: Request, res: Response, next: NextFunction) => {
-      
+    try {
+      const command:string = "INSERT INTO bytes ($1 , $2, $3)"
+      const 
+    }catch(err:any) {
+      const error:T.error = {
+      message:`/controllers/dbController createUser error ${typeof err === 'object' ? JSON.stringify(err) : err}`,
+        status: 500,
+        log: 'database error check server log'
+      }
+      return next(error)
+    }   
   },
-  getHash: async (req: Request, res: Response, next: NextFunction) => {
+  getHash: async (req: Request, res: Response, next: NextFunction):Promise<any> => {
+    try {
       const { username } = req.body;
-      const query = "SELECT password FROM WHERE username = $1;";
-      db.query(query,[username])
+      const query = "SELECT password FROM bytes WHERE username = $1;";
+      const password = await db.query(query,[username])
+      res.locals.passowrd = password;
+      return next();
+    }catch(err) {
+      const error:T.error = {
+        message:`/controllers/dbController getHash error ${typeof err === 'object' ? JSON.stringify(err) : err}`,
+        status: 500,
+        log: 'database error check server log'
+      }
+      return next(error)
+    }
   },
 }
--- server/type.ts
+++ server/type.ts
@@ -2,3 +2,13 @@
 export interface env {
   connectionString: string
 }
+
+export interface user {
+  
+}
+
+export interface error {
+  log:string,
+  message: string,
+  status: number
+}
