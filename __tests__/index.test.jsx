c08907b25a188ea6e598c848137d84aff9b81a74
Nancy Zhang <zhangn356@gmail.com>
2023-03-07 15:45:35 -0500

    starting tests

---
+++ __tests__/index.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Feed from '../client/src/components/Feed.jsx' 
+
+
+describe ('Feed', () => {
+    it ('renders a heading', () => {
+        render (<Feed/>);
+        const heading = screen.getByRole('heading', {
+            name: 'Feed'
+        })
+    expect(heading).toBeInTheDocument();
+    })
+})
