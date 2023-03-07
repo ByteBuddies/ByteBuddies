ae1b97415cc7a626ab822bdab830381ef59ee2b5
ctolmsted <prunes03smugger@icloud.com>
2023-03-07 15:03:02 -0500

    flex architecture for login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import loginPic from '../../assets/login.png';
 
 const Login = () => {
-    return (
-        <>
-            <h1>Byte Buddies</h1>
-            <form>
-                <h2>Login</h2>
-                <input></input>
-                <input></input>
-                <button>Log In</button>
-                <button>Sign Up</button>
-            </form>
-        </>
-    );
-  };
-  
-  export default Login;
+  return (
+    <div class="flex-container">
+      <div id="image-container">
+        <img id="login-sidebar-image" src={loginPic}></img>
+      </div>
+      <div id="login-container">
+        <div id="login-group">
+          <h1>Byte Buddies</h1>
+          <form id="login-form">
+            <h2>Login</h2>
+            <input class="login-input"></input>
+            <input class="login-input"></input>
+            <button>Log In</button>
+            <Link to="/">Sign Up</Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
