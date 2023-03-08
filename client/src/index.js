9e72858d321e550b29b0b11b8ce623d0803ddadc
Nancy Zhang <zhangn356@gmail.com>
2023-03-07 19:41:27 -0500

    tested routed components

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
 import Login from "./pages/Login.jsx";
-import Main from "./pages/Main.js";
+import Main from "./pages/Main.jsx";
 import '../styles.scss';
 
 const domNode = document.getElementById('root');
