dc6bdc6c73b9f88e87faf5b84dcf182295ac72c5
ctolmsted <prunes03smugger@icloud.com>
2023-03-07 19:30:43 -0500

    implemented DOS theme for main page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import { Profile } from "../components/Profile.jsx";
-import { Feed } from "../components/Feed.jsx";
+import React from 'react';
+import { Profile } from '../components/Profile.jsx';
+import { Feed } from '../components/Feed.jsx';
 
 export const Home = () => {
-    return (
-      <>
-        <div className="home-container">
-          <Profile id="profile-window"/>
-          <Feed id="feed-window"/>
-        </div>
-      </>  
-    );
-}
+  return (
+    <>
+      <div className="home-container">
+        <section className="home-faux-screen-border">
+          <section className="home-faux-screen-container">
+            <Profile />
+            <Feed />
+          </section>
+        </section>
+      </div>
+    </>
+  );
+};
--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React from 'react';
+import scoob from '../../assets/Characters/bear.png'
 
 export const Profile = () => {
-    return (
-      <>
-        <div className="profile-container">
-            <div className="profile-header">
-                <img></img>
-                <h1>UserName</h1>
-            </div>
-            <div className="profile-body">
-                <p>Name</p>
-                <p>My Skills</p>
-                <p>Sought Skills</p>
-                <p>Current Project</p>
-                <p>LinkedIn</p>
-                <p>GitHub</p>
-            </div>
+  return (
+    <>
+      <div className="profile-container">
+        <div className="profile-header">
+          <div class="profile-photo-container">
+            <img class="profile-photo" src={scoob}></img>
+          </div>
+          <h1>UserName</h1>
         </div>
-      </>  
-    );
-}
+        <div className="profile-body">
+          <p>Name</p>
+          <p>My Skills</p>
+          <p>Sought Skills</p>
+          <p>Current Project</p>
+          <p>LinkedIn</p>
+          <p>GitHub</p>
+        </div>
+      </div>
+    </>
+  );
+};
