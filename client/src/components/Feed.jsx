f37fbed715109aba4b2e01575cbe59443c5b8c08
ctolmsted <prunes03smugger@icloud.com>
2023-03-08 15:31:58 -0500

    added photos to feed

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,18 +1,24 @@
-import React from "react";
-import { UserCard } from "../components/UserCard.jsx";
+import React from 'react';
+import { UserCard } from '../components/UserCard.jsx';
 
 export const Feed = () => {
-    // logic here for amount of user cards needed
-    return (
-      <>
-        <div className="feed-container">
-            <h1>Feed</h1>
-            <div className="card-container">
-                <UserCard/>
-                <UserCard/>
-                <UserCard/>
-            </div>
-        </div>
-      </>  
-    );
-}
+  // logic here for amount of user cards needed
+  return (
+    <>
+      <div className="feed-container">
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+      </div>
+    </>
+  );
+};
--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React from 'react';
+import shaggy from '../../assets/Characters/shaggy.png'
 
 export const UserCard = () => {
-    return (
-      <>
-        <div className="card">
-            <img></img>
-            <div className="card-body">
-                <p>Username</p>
-                <p>Match %</p>
-                <p>Skills</p>
-            </div>
+  return (
+    <>
+      <div className="card">
+        <div class="profile-photo-container">
+          <img class="profile-photo" src={shaggy}></img>
         </div>
-
-      </>  
-    );
-}
+        <div className="card-body">
+          <p>Username</p>
+          <p>Match %</p>
+          <p>Skills</p>
+        </div>
+      </div>
+    </>
+  );
+};
